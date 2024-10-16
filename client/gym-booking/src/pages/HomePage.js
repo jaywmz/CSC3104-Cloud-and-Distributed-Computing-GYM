44a94dafe5415f0b64cdb5bf9c37de3b7b37d80e
Felix Chang <2014.felixchang@gmail.com>
2024-10-16 15:40:16 +0800

    feat(booking): add 'confirm booking' modal for booking (will help with concurrency later on)

--- client/gym-booking/src/pages/HomePage.js
+++ client/gym-booking/src/pages/HomePage.js
@@ -6,11 +6,11 @@ const HomePage = () => {
         <div className="col-md-4">
             <div className="card shadow-lg p-3 mb-5 bg-body rounded">
                 <div className="card-body">
-                    <h2 className="card-title text-center mb-4">Gym Starting Page</h2>
+                    <h1 className="card-title text-center mb-4">GymKube</h1>
+                    <h3 className="text-center mb-4" >Welcome to GymKube Web Portal</h3>
                     <div className="text-center">
                         <a href="/login" className="btn btn-primary w-100 d-block mb-2">Login</a>
                         <a href="/register" className="btn btn-secondary w-100 d-block">Register</a>
-                        <a href="/bookings" className="btn btn-success w-100 d-block mt-2">Bookings</a>
                     </div>
                 </div>
             </div>
